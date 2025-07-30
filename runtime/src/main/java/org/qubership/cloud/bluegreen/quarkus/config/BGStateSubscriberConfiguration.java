974e134c12bbe78d6c27d7f40d3a3a2804551f2a
larkin <larkin.qubership@gmail.com>
2025-07-30 11:45:30 +0400

    docs: javadocs were added

--- runtime/src/main/java/org/qubership/cloud/bluegreen/quarkus/config/BGStateSubscriberConfiguration.java
+++ runtime/src/main/java/org/qubership/cloud/bluegreen/quarkus/config/BGStateSubscriberConfiguration.java
@@ -8,6 +8,26 @@ import org.jboss.logging.Logger;
 import org.qubership.cloud.bluegreen.api.model.BlueGreenState;
 import org.qubership.cloud.bluegreen.api.service.BlueGreenStatePublisher;
 
+/**
+ * Subscribes to Blue-Green state changes at application startup and updates a system property with the current state.
+ * <p>
+ * This class is a Quarkus {@code @ApplicationScoped} bean that listens to the {@link StartupEvent}
+ * and registers a callback with the {@link BlueGreenStatePublisher} (if available).
+ * Whenever the deployment state changes, the new state is written to a system property
+ * defined by {@link #BG_STATE_SYSTEM_PROPERTY_KEY}.
+ * </p>
+ *
+ * <p>
+ * This system property (key: {@code "bgState"}) can be used by logging frameworks or other components
+ * that require access to the current deployment state but are not directly integrated with the
+ * Blue-Green infrastructure.
+ * </p>
+ *
+ * <p>Example usage in {@code application.properties}:</p>
+ * <pre>{@code
+ * quarkus.log.console.format=[%d{yyyy-MM-dd'T'HH:mm:ss.SSS}][%-5p] [bgState:%#{bgState:unknown}] %s%e%n
+ * }</pre>
+ */
 @ApplicationScoped
 public class BGStateSubscriberConfiguration {
     public static final String BG_STATE_SYSTEM_PROPERTY_KEY = "bgState";
@@ -18,7 +38,7 @@ public class BGStateSubscriberConfiguration {
 
     void onStart(@Observes StartupEvent ev) {
         if (blueGreenStatePublisher != null) {
-            log.info("Subscribe to BlueGreenState change event => store BlueGreenState to the System property");
+            log.info("Subscribe to BlueGreenState change event => store BlueGreenState to the 'bgState' System property");
             blueGreenStatePublisher.subscribe(this::setBGStateToSystemProperties);
         } else {
             log.warn("Cannot get BlueGreenStatePublisher bean -> skip subscription");
